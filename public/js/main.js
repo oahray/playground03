acfd4624014c4c63679383b59423b0e7577df822
Oahray <rodneyoare@gmail.com>
2017-05-14 20:02:27 +0100

    Initial commit

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+      $("#quote").typed({
+        strings: ["you can also call me Ray.", "I am proudly Nigerian.", "design is my game.", "I know I'm awesome.", "this site is under construction."],
+        typeSpeed: 70,
+        loop : true
+      });
+  });
